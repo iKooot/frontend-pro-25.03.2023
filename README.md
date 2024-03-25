5caeaddc1317fe293d6d57d084670755730e791d
iKot <sergey.savchuk91@gmail.com>
2024-03-25 21:27:22 +0200

    added lesson 1

---
+++ README.md
@@ -0,0 +1,12 @@
+# Hillel frontend pro course 02.10.2023
+## Lector - [Serhii Savchuk](https://www.linkedin.com/in/sergey-savchuk-fronend/)
+
+### Structure of repo
+Each lesson we will create new folder with number of lesson. For example: **'Lesson1'**.
+
+This folder contains:
+1. Code folder - with code from lesson;
+2. Homework folder - with home tasks;
+3. Assets folder - with different files, including presentation.
+
+#### [Program of course](https://lms.ithillel.ua/groups/64e8915b120d1c4d470261b5/program)
---
+++ lesson1/code/index.js
@@ -0,0 +1,72 @@
+/*
+    alert('Hello world');
+    alert('Hello world2');
+    [1, 2].forEach(alert);
+ */
+
+
+// const a = 1;
+// const b = 1;
+
+// const h1 = document.querySelector('h1');
+// h1.style.color = 'red'
+// console.log(h1)
+
+/****************************************
+ * strict mode
+ ****************************************/
+// 'use strict'
+
+
+/****************************************
+ * declaration
+ ****************************************/
+
+var a = 1; // Legacy Dont do it
+
+let b = 1; // writable
+const c = 1; // readable
+
+/****************************************
+ * string
+ ****************************************/
+
+const example1 = 't'
+const example2 = "t"
+const example3 = `t`
+
+/****************************************
+ * math operators
+ ****************************************/
+
+const operators = '+ - * / % ++ --'
+
+/****************************************
+ * syntax variable
+ ****************************************/
+
+const camelCase = 'exampleTextValue'
+const snake_ase = 'example_text_value'
+// const MONGO_URI = 'mongo/text'
+const PascaleCase = 'ExampleTextValue'
+// const cebab-case = '' // Error
+// class MyClass {}
+
+/****************************************
+ * function
+ ****************************************/
+
+console.log('before')
+alert('Hello world')
+console.log('after')
+
+const message = prompt('How old are you?', 0);
+console.log(message);
+
+const result = confirm('Are you happy?');
+console.log(result)
+
+
+
+
+
